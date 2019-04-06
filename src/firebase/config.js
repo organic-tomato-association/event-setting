c15fdd050b3b359a75ef64b358ecb2a338505a42
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2019-04-07 02:20:00 +0900

    [delete] config.js

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: 'AIzaSyBsmKZwVnFLqr_dRroJ4EXHAiB8-SlTnCw',
-    authDomain: 'event-setting.firebaseapp.com',
-    databaseURL: 'https://event-setting.firebaseio.com',
-    projectId: 'event-setting',
-    storageBucket: 'event-setting.appspot.com',
-    messagingSenderId: '925199588708',
-};
